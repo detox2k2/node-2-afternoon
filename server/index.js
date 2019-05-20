1bda70d4a387b778a8cd6b8496bf5c8546a096c3
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-05-20 15:05:04 -0500

    afternoon done

--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const mc = require('./controllers/messages_controller');
-
 const app = express();
-
-const {json} = require('body-parser');
-
 app.use(express.json());
 app.use(express.static(__dirname + '/../public/build'));
 
-const messageBaseUrl = "/api/messages";
-app.post(messageBaseUrl, mc.create);
-app.get(messageBaseUrl, mc.read);
-app.put(`${messageBaseUrl}/:id`, mc.update);
-app.delete(`${messageBaseUrl}/:id`, mc.delete);
+const messagesBaseUrl = '/api/messages'
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+
 
 
 const port = 3001;
 app.listen(port, () => {
-    console.log('server listening on port ${port}');
-});
+console.log('colder than a polar bear toenails')
+})
+
 
